b5c2ed978c9bdf03ccb9fecfab64f1530088a27b
cristiandisilvestre <cristian.disilvestre98@gmail.com>
2023-05-25 14:05:44 +0200

    Fixed movemet weapon

--- Assets/Scripts/Game/AnimationController.cs
+++ Assets/Scripts/Game/AnimationController.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class AnimationController : MonoBehaviour
 {
-
     private float _epsilon = 0.05f;
     public void FlippingCharacter(Rigidbody2D rigidbody) {
         // Flipping image
         Vector3 _currentScale = rigidbody.transform.localScale;
         if (rigidbody.velocity.x * _currentScale.x < 0) {
             rigidbody.transform.localScale = new Vector3(
-                -_currentScale.x, _currentScale.y, _currentScale.z);
+                -_currentScale.x, _currentScale.y, _currentScale.z);  
         }
     }
     public void MoveAnimations(Rigidbody2D rigidbody, Animator animator) {
