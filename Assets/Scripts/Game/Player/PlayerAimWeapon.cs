3ea398f1c9494610b07d93f0688637726cc8afa8
cristiandisilvestre <cristian.disilvestre98@gmail.com>
2023-05-25 14:05:44 +0200

    Fixed movemet weapon

--- Assets/Scripts/Game/Player/PlayerAimWeapon.cs
+++ Assets/Scripts/Game/Player/PlayerAimWeapon.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerAimWeapon : MonoBehaviour {
-    private Vector3 _mousePos;
+    public Vector3 _mousePos;
     private Transform _aimTransform;
+
+    private Rigidbody2D _rigidBody;
    
     private void Awake() {
         _aimTransform = transform.Find("Aim");
+        _rigidBody = GetComponent<Rigidbody2D>();
     }
 
     private void Update() {
@@ -19,6 +22,10 @@ public class PlayerAimWeapon : MonoBehaviour {
     private void HandleAiming() {
         
         _mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        
+        if(_rigidBody.transform.localScale.x<0){
+            _mousePos = new Vector3(-_mousePos.x, -_mousePos.y, _mousePos.z);
+        }
         Vector3 aimDirection = (_mousePos - transform.position).normalized;
         float angle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
         _aimTransform.eulerAngles= new Vector3(0, 0, angle);
