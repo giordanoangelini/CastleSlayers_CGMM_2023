2e1216ccb68bb27bd34ce6488eea9f4d794a9795
Giordano <giordano@example.com>
2023-05-26 13:11:31 +0200

    fix shoot direction

--- Assets/Scripts/Game/Player/PlayerShoot.cs
+++ Assets/Scripts/Game/Player/PlayerShoot.cs
@@ -9,28 +9,40 @@ public class PlayerShoot : MonoBehaviour
     [SerializeField] private float _bulletSpeed;
     [SerializeField] private Transform _fireSpot;
     [SerializeField] private float _timeBetweenShots;
+    [SerializeField] private Transform _weapon;
     private float _lastFireTime;
     private bool _fireContinuously;
+    private bool _fireSingle;
+    private Animator _animator;
     
+    private void Awake() {
+        _animator = _weapon.GetComponent<Animator>();
+    }
     void Update()
     {
-        if (_fireContinuously) {
+        /*
+        if (_fireContinuously || _fireSingle) {
             float timeSinceLastFire = Time.time - _lastFireTime;
             if (timeSinceLastFire > _timeBetweenShots) {
                 FireBullet();
                 _lastFireTime = Time.time;
+                _fireSingle = false;
             }
-        }
+        }*/
     }
 
     private void FireBullet()
     {
-        GameObject bullet = Instantiate(_bulletPrefab, _fireSpot.transform.position, transform.rotation);
+        _animator.SetTrigger("shoot");
+        GameObject bullet = Instantiate(_bulletPrefab, _fireSpot.transform.position, _fireSpot.transform.rotation);
         Rigidbody2D rigidbody = bullet.GetComponent<Rigidbody2D>();
         rigidbody.velocity = _bulletSpeed * _fireSpot.transform.right;
     }
     private void OnFire(InputValue inputValue)
     {
-        _fireContinuously = inputValue.isPressed;
+        //_fireContinuously = inputValue.isPressed;
+        if (inputValue.isPressed) {
+            FireBullet();
+        }
     }
 }
