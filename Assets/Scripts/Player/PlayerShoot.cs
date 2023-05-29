51028215706e03b4fc838d3c5aac74d45d0845c1
Giordano <giordano@example.com>
2023-05-29 11:59:21 +0200

    logica aim and shoot

--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -5,43 +5,25 @@ using UnityEngine.InputSystem;
 
 public class PlayerShoot : MonoBehaviour
 {
-    [SerializeField] private GameObject _bulletPrefab;
-    [SerializeField] private float _bulletSpeed;
     private Transform _fireSpot;
-    [SerializeField] private float _timeBetweenShots;
-    [SerializeField] private Transform _weapon;
+    private Transform _weapon;
     private float _lastFireTime;
-    private bool _fireContinuously;
-    private bool _fireSingle;
-    private Animator _animator;
+    private WeaponParameters _weaponParameters;
     
     private void Awake() {
-        _animator = _weapon.GetComponent<Animator>();
-        _fireSpot = _weapon.Find("FireSpot");
-    }
-    void Update()
-    {
-        /*
-        if (_fireContinuously || _fireSingle) {
-            float timeSinceLastFire = Time.time - _lastFireTime;
-            if (timeSinceLastFire > _timeBetweenShots) {
-                FireBullet();
-                _lastFireTime = Time.time;
-                _fireSingle = false;
-            }
-        }*/
+        _weapon = transform.Find("Weapon");
+        _fireSpot = _weapon.transform.Find("FireSpot");
+        _weaponParameters = _weapon.GetComponent<WeaponParameters>();
     }
 
     private void FireBullet()
     {
-        _animator.SetTrigger("shoot");
-        GameObject bullet = Instantiate(_bulletPrefab, _fireSpot.transform.position, new Quaternion());
+        GameObject bullet = Instantiate(_weaponParameters.bulletPrefab, _fireSpot.transform.position, new Quaternion());
         Rigidbody2D rigidbody = bullet.GetComponent<Rigidbody2D>();
-        rigidbody.velocity = _bulletSpeed * _fireSpot.transform.right;
+        rigidbody.velocity = _weaponParameters.bulletSpeed * _fireSpot.transform.right;
     }
     private void OnFire(InputValue inputValue)
     {
-        //_fireContinuously = inputValue.isPressed;
         if (inputValue.isPressed) {
             FireBullet();
         }
---
+++ Assets/Scripts/Player/WeaponParameters.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponParameters : MonoBehaviour
+{
+    [SerializeField] public GameObject bulletPrefab;
+    [SerializeField] public float bulletSpeed;
+    [SerializeField] public float timeBetweenShots;
+}
