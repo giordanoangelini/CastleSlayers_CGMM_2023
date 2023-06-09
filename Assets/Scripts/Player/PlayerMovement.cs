9749ee253a6a42d05e692cef2c57beaaeee0366c
cristiandisilvestre <cristian.disilvestre98@gmail.com>
2023-06-09 12:39:26 +0200

    Initial sound

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,6 +7,10 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _speed;
+    [SerializeField] private AudioSource _walkSound;
+    public float lastSoundTime {get; set;}
+    private float _epsilon = 0.1f;
+    private float _timeBetweenSound = 0.3f;
     private Rigidbody2D _rigidBody;
     private Vector2 _movementInput;
     private Vector2 _smoothedMovementInput;
@@ -16,6 +20,7 @@ public class PlayerMovement : MonoBehaviour
     private void Awake() {
         _rigidBody = GetComponent<Rigidbody2D>();
         _animator = GetComponent<Animator>();
+        lastSoundTime = Time.time;
     }
 
     private void FixedUpdate() {
@@ -32,6 +37,18 @@ public class PlayerMovement : MonoBehaviour
             0.1f
         );
         _rigidBody.velocity = _smoothedMovementInput * _speed;
+        Debug.Log(_rigidBody.velocity.x);
+        Debug.Log(_rigidBody.velocity.y);
+        if((Math.Abs(_rigidBody.velocity.x) > _epsilon || Math.Abs(_rigidBody.velocity.y) > _epsilon) && CanPlaySound()){
+            _walkSound.Play();
+            lastSoundTime = Time.time;
+        }
+    }
+
+    private bool CanPlaySound() {
+        float timeSinceLastSound = Time.time - lastSoundTime;
+        if (timeSinceLastSound >= _timeBetweenSound) return true;
+        return false;
     }
 
     private void OnMove(InputValue inputValue) {
