d1eadcc68285e09a3df092f50d9ba28053850d5c
Giordano <angelinigiordano@gmail.com>
2023-06-14 21:51:56 +0200

    input field

--- Assets/Scripts/Menu/OptionsMenu.cs
+++ Assets/Scripts/Menu/OptionsMenu.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
+    private void Awake() {
+        FindAnyObjectByType<InputField>().text = PlayerPrefs.GetString("username");
+    }
     public void SetVolume(float volume) {
         Debug.Log(volume);
+    }
+
+    public void SetUsername (string username) {
+        Debug.Log(username);
+        PlayerPrefs.SetString("username", username);
     }
 }
