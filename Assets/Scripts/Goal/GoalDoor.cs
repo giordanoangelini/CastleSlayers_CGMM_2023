61104b60c402bb1268276ba3f32f9ef6c623836d
Giordano <angelinigiordano@gmail.com>
2023-06-14 12:11:52 +0200

    quick fix

---
+++ Assets/Scripts/Goal/GoalDoor.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Tilemaps;
+
+public class GoalDoor : MonoBehaviour
+{
+    private void FixedUpdate() {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("enemy");
+        if (enemies.Length == 0) Destroy(gameObject);
+    }
+}
