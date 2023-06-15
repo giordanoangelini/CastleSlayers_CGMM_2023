da48adcec420c348fad08df6e46c0a948a045ae0
cristiandisilvestre <cristian.disilvestre98@gmail.com>
2023-06-15 16:31:05 +0200

    Door

--- Assets/Scripts/Goal/GoalDoor.cs
+++ Assets/Scripts/Goal/GoalDoor.cs
@@ -8,6 +8,9 @@ public class GoalDoor : MonoBehaviour
 {
     private void FixedUpdate() {
         GameObject[] enemies = GameObject.FindGameObjectsWithTag("enemy");
-        if (enemies.Length == 0) Destroy(gameObject);
+        if (enemies.Length == 0) {
+            Destroy(gameObject);
+            Destroy(GameObject.FindGameObjectWithTag("door"));
+        }
     }
 }
